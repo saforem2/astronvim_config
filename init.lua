badb5b863805871d28dc17914561498a0fee11bf
Sam Foreman <saforem2@gmail.com>
2023-04-28 10:35:24 -0500

    Update colorscheme

--- init.lua
+++ init.lua
@@ -1,24 +1,41 @@
 return {
   -- Configure AstroNvim updates
   updater = {
-    remote = "origin", -- remote to use
-    channel = "stable", -- "stable" or "nightly"
-    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "nightly", -- branch name (NIGHTLY ONLY)
-    commit = nil, -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false, -- skip prompts about breaking changes
+    remote = "origin",     -- remote to use
+    channel = "stable",    -- "stable" or "nightly"
+    version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    branch = "nightly",    -- branch name (NIGHTLY ONLY)
+    commit = nil,          -- commit hash (NIGHTLY ONLY)
+    pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
+    skip_prompts = false,  -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false, -- automatically quit the current session after a successful update
-    remotes = { -- easily add new remotes to track
+    auto_quit = false,     -- automatically quit the current session after a successful update
+    remotes = {            -- easily add new remotes to track
       --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
       --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
     },
   },
 
+  -- -- Apply custom highlights on colorscheme change.
+  -- -- Must be declared before executing ':colorscheme'.
+  -- local grpid = vim.api.nvim_create_augroup('custom_highlights_sonokai', {})
+  -- vim.api.nvim_create_autocmd('ColorScheme', {
+  --   group = grpid,
+  --   pattern = 'sonokai',
+  --   callback = function()
+  --     local config = vim.fn['sonokai#get_configuration']()
+  --     local palette = vim.fn['sonokai#get_palette'](config.style, config.colors_override)
+  --     local set_hl = vim.fn['sonokai#highlight']
+  --
+  --     set_hl('Visual', palette.none, palette.black)
+  --     set_hl('IncSearch', palette.bg0, palette.yellow)
+  --     set_hl('Search', palette.none, palette.diff_yellow)
+  --   end
+  -- })
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "oxocarbon",
+
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
@@ -31,7 +48,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true, -- enable or disable format on save globally
+        enabled = true,     -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
