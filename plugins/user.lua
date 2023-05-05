679ba6e27480dbe42018de958e960e83aab114ec
Sam Foreman <saforem2@gmail.com>
2023-05-05 07:57:24 -0500

    Update `plugins/*.lua`

--- plugins/user.lua
+++ plugins/user.lua
@@ -443,5 +443,36 @@ return {
         end,
       })
     end
-  }
+  },
+  {
+    "norcalli/nvim-terminal.lua",
+    config = function()
+      require('terminal').setup()
+    end
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    config = function()
+      local rt = require('rust-tools')
+      rt.setup({
+        server = {
+          on_attach = function(_, bufnr)
+            -- Hover actions
+            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+          end,
+        },
+      })
+    end,
+  },
+  {
+    "lvimuser/lsp-inlayhints.nvim"
+  },
+  {
+    "preservim/vim-markdown"
+  },
+  {
+    "HiPhish/nvim-ts-rainbow2"
+  },
 }
