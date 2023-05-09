84e3b80a80737a7390e03d13363bd93b61f5d273
Sam Foreman <saforem2@gmail.com>
2023-05-08 19:31:32 -0500

    Update `plugins/core.lua`

--- plugins/core.lua
+++ plugins/core.lua
@@ -20,6 +20,8 @@ return {
       return opts
     end,
   },
+  { "NvChad/nvim-colorizer.lua", enabled = false },
+
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
   --
