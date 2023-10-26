f555937a9c87f9d8ec8f77027be4c8ca519b8a98
Sam Foreman <saforem2@gmail.com>
2023-10-26 14:53:48 -0500

    Create `plugins/mini.lua`

---
+++ plugins/mini.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    'echasnovski/mini.animate',
+    config = function()
+      require('mini.animate').setup()
+    end,
+  },
+  {
+    'echasnovski/mini.colors',
+    config = function()
+      require('mini.colors').setup()
+    end,
+  },
+  -- {
+  --   "echasnovski/mini.indentscope",
+  --   version = false, -- wait till new 0.7.0 release to put it back on semver
+  --   event = "LazyFile",
+  --   opts = {
+  --     -- symbol = "▏",
+  --     symbol = "│",
+  --     options = { try_as_border = true },
+  --   },
+  --   init = function()
+  --     vim.api.nvim_create_autocmd("FileType", {
+  --       pattern = {
+  --         "help",
+  --         "alpha",
+  --         "dashboard",
+  --         "neo-tree",
+  --         "Trouble",
+  --         "lazy",
+  --         "mason",
+  --         "notify",
+  --         "toggleterm",
+  --         "lazyterm",
+  --       },
+  --       callback = function()
+  --         vim.b.miniindentscope_disable = true
+  --       end,
+  --     })
+  --   end,
+  -- },
+}
