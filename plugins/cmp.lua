dfc2d33041246e4a48fb667c722f8d12b3793d37
Sam Foreman <saforem2@gmail.com>
2023-11-02 17:22:51 -0500

    Update `plugins/cmp.lua`

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -6,7 +6,16 @@ return {
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
-      ['<CR>'] = cmp.mapping.confirm({ select = false }),
+      -- ['<CR>'] = cmp.mapping.confirm({ select = false }),
+      -- ["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert}),
+      ['<CR>'] = function(fallback)
+        if cmp.visible() then
+          cmp.confirm()
+        else
+          fallback() -- If you use vim-endwise, this fallback will behave the same as vim-endwise.
+        end
+      end
+
     }),
   }),
 }
