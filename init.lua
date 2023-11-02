76db990cf90c9d4d3cce600f0ca44910d944d308
Sam Foreman <saforem2@gmail.com>
2023-11-02 17:22:00 -0500

    Update `init.lua`

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-
 return {
   -- Configure AstroNvim updates
   updater = {
@@ -66,68 +65,84 @@ return {
     -- set highlights for all themes
     -- use a function override to let us use lua to retrieve colors from highlight group
     -- there is no default table so we don't need to put a parameter for this function
-    init = function()
-      local get_hlgroup = require("astronvim.utils").get_hlgroup
-      -- get highlights from highlight groups
-      local normal = get_hlgroup "Normal"
-      local fg, bg = normal.fg, normal.bg
-      local bg_alt = get_hlgroup("Visual").bg
-      local green = get_hlgroup("String").fg
-      local red = get_hlgroup("Error").fg
-      -- return a table of highlights for telescope based on colors gotten from highlight groups
-      return {
-        TelescopeBorder = { fg = bg_alt, bg = bg },
-        TelescopeNormal = { bg = bg },
-        TelescopePreviewBorder = { fg = bg, bg = bg },
-        TelescopePreviewNormal = { bg = bg },
-        TelescopePreviewTitle = { fg = bg, bg = green },
-        TelescopePromptBorder = { fg = bg_alt, bg = bg_alt },
-        TelescopePromptNormal = { fg = fg, bg = bg_alt },
-        TelescopePromptPrefix = { fg = red, bg = bg_alt },
-        TelescopePromptTitle = { fg = bg, bg = red },
-        TelescopeResultsBorder = { fg = bg, bg = bg },
-        TelescopeResultsNormal = { bg = bg },
-        TelescopeResultsTitle = { fg = bg, bg = bg },
-      }
-    end,
+    -- init = function()
+    --   local get_hlgroup = require("astronvim.utils").get_hlgroup
+    --   -- get highlights from highlight groups
+    --   local normal = get_hlgroup "Normal"
+    --   local fg, bg = normal.fg, normal.bg
+    --   local bg_alt = get_hlgroup("Visual").bg
+    --   local green = get_hlgroup("String").fg
+    --   local red = get_hlgroup("Error").fg
+    --   -- return a table of highlights for telescope based on colors gotten from highlight groups
+    --   return {
+    --     TelescopeBorder = { fg = bg_alt, bg = bg },
+    --     TelescopeNormal = { bg = bg },
+    --     TelescopePreviewBorder = { fg = bg, bg = bg },
+    --     TelescopePreviewNormal = { bg = bg },
+    --     TelescopePreviewTitle = { fg = bg, bg = green },
+    --     TelescopePromptBorder = { fg = bg_alt, bg = bg_alt },
+    --     TelescopePromptNormal = { fg = fg, bg = bg_alt },
+    --     TelescopePromptPrefix = { fg = red, bg = bg_alt },
+    --     TelescopePromptTitle = { fg = bg, bg = red },
+    --     TelescopeResultsBorder = { fg = bg, bg = bg },
+    --     TelescopeResultsNormal = { bg = bg },
+    --     TelescopeResultsTitle = { fg = bg, bg = bg },
+    --   }
+    -- end,
   },
+  -- require("neoconf").setup({}),
 
-  posh = function()
-    vim.opt.spell = false
-    vim.opt.termguicolors = true
-    -- vim.opt.backupdir = { function(local_vim) return { local_vim.fn.expand('~/.backup//') } end }
-    vim.opt.undodir = function(local_vim) return { local_vim.fn.expand('~/.undo//') } end
+  polish = function()
+    -- vim.opt.spell = false
+    -- vim.opt.termguicolors = true
+    -- vim.g['pandoc#filetypes#handled'] = {'pandoc', 'rst', 'textile', 'markdown', 'quarto'}
+    -- vim.g.pandoc.filetypes.handled = {'pandoc', 'rst', 'textile', 'markdown', 'quarto'}
+    -- g:pandoc#filetypes#handled = ['pandoc', 'rst', 'textile', 'markdown', 'quarto']
+    -- vim.opt.spli
+    vim.opt.splitright = true
+    vim.opt.splitbelow = true
+    vim.opt.backupdir = function(local_vim)
+      return local_vim.fn.expand('~/.backup//')
+    end
+    vim.opt.undodir = function(local_vim)
+      return local_vim.fn.expand('~/.undo//')
+    end
+    -- end
     -- vim.keymap.set("c", "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, { desc = "Redirect Cmdline" })
-    vim.api.nvim_create_autocmd({ "BufEnter" }, {
-        pattern = { "*.md" },
-        callback = function()
-            local otter = require('otter')
-            otter.activate({ 'r', 'python', 'lua' }, true)
-            vim.api.nvim_buf_set_keymap(0, 'n', 'gd', ":lua require'otter'.ask_definition()<cr>", { silent = true })
-            vim.api.nvim_buf_set_keymap(0, 'n', 'K', ":lua require'otter'.ask_hover()<cr>", { silent = true })
-            vim.api.nvim_buf_set_keymap(0, 'n', '<C-c>', ":lua require(notify).dismiss()<cr>", { silent = false })
-        end,
-    })
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile", "BufFilePre", "BufRead" }, {
-        pattern = { "*.md" },
-        callback = function()
-            -- vim.api.nvim_eval('let filetype=markdown.pandoc')
-            vim.api.nvim_set_option_value('filetype', 'markdown.pandoc', {})
-            vim.g.vim_markdown_auto_insert_bullets = 0
-            vim.g.vim_markdown_new_list_item_indent = 0
-        end
-   })
-
-    vim.api.nvim_create_autocmd({ "BufEnter" }, {
-        pattern = { "*.md" },
-        callback = function()
-            local otter = require('otter')
-            otter.activate({ 'r', 'python', 'lua' }, true)
-            vim.api.nvim_buf_set_keymap(0, 'n', 'gd', ":lua require'otter'.ask_definition()<cr>", { silent = true })
-            vim.api.nvim_buf_set_keymap(0, 'n', 'K', ":lua require'otter'.ask_hover()<cr>", { silent = true })
-            vim.api.nvim_buf_set_keymap(0, 'n', '<C-c>', ":lua require(notify).dismiss()<cr>", { silent = false })
-        end,
-    })
+    -- augroup pandoc_syntax
+    --   au! BufNewFile,BufFilePre,BufRead *.md set filetype=markdown.pandoc
+    --   au! BufNewFile,BufFilePre,BufRead *.qmd set filetype=markdown.pandoc
+    -- augroup END
+  --   vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  --     pattern = { "*.md", "*.qmd" },
+  --     callback = function()
+  --       local otter = require('otter')
+  --       otter.activate({ 'r', 'python', 'lua' }, true)
+  --       vim.api.nvim_buf_set_keymap(0, 'n', 'gd', ":lua require'otter'.ask_definition()<cr>", { silent = true })
+  --       vim.api.nvim_buf_set_keymap(0, 'n', 'K', ":lua require'otter'.ask_hover()<cr>", { silent = true })
+  --       vim.api.nvim_buf_set_keymap(0, 'n', '<C-c>', ":lua require(notify).dismiss()<cr>", { silent = false })
+  --     end,
+  --   })
+  --   vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile", "BufFilePre", "BufRead" }, {
+  --       pattern = { "*.md", "*.qmd" },
+  --       callback = function()
+  --           -- vim.api.nvim_eval('let filetype=markdown.pandoc')
+  --           vim.api.nvim_set_option_value('filetype', 'markdown.pandoc', {})
+  --           vim.g.vim_markdown_auto_insert_bullets = 0
+  --           vim.g.vim_markdown_new_list_item_indent = 0
+  --       end
+  --  })
+  --
+  --   vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  --       pattern = { "*.md", "*.qmd", "quarto" },
+  --       callback = function()
+  --           local otter = require('otter')
+  --           otter.activate({ 'r', 'python', 'lua' }, true)
+  --           vim.api.nvim_buf_set_keymap(0, 'n', 'gd', ":lua require'otter'.ask_definition()<cr>", { silent = true })
+  --           vim.api.nvim_buf_set_keymap(0, 'n', 'K', ":lua require'otter'.ask_hover()<cr>", { silent = true })
+  --           vim.api.nvim_buf_set_keymap(0, 'n', '<C-c>', ":lua require(notify).dismiss()<cr>", { silent = false })
+  --       end,
+  --   })
   end,
 }
 
