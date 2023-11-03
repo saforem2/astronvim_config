8b1c1fddfb28872f50cdaf00b2cd96a1d99e891b
Sam Foreman <saforem2@gmail.com>
2023-11-03 10:08:47 -0500

    Update config

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,10 +1,51 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    if not opts.ensure_installed then opts.ensure_installed = {} end
-    require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
-    })
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        -- "lua"
+      })
+    end,
+  },
+  {
+    'hiphish/rainbow-delimiters.nvim',
+    config = function()
+      require("rainbow-delimiters.setup").setup({
+        local_rainbow_delimiters = require 'rainbow-delimiters',
+        strategy = {
+          [''] = require('rainbow-delimiters').strategy['global'],
+          vim = require('rainbow-delimiters').strategy['local'],
+        },
+        query = {
+          [''] = 'rainbow-delimiters',
+          lua = 'rainbow-blocks',
+        },
+        highlight = {
+          'RainbowDelimiter00',
+          'RainbowDelimiter01',
+          'RainbowDelimiter02',
+          'RainbowDelimiter03',
+          'RainbowDelimiter04',
+          'RainbowDelimiter05',
+          'RainbowDelimiter06',
+        },
+        -- local hooks = require "ibl.hooks",
+        -- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        --     vim.api.nvim_set_hl(0, 'RainbowDelimiter00', { bg = "#E06C75" })
+        --     vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        --     vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        --     vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        --     vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        --     vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        --     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+        --   end),
+        -- vim.g.rainbow_delimiters = { highlight = highlight },
+        -- require("ibl").setup({ scope = { highlight = highlight } }),
+        -- hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+      })
+    end,
+  },
+}
 }
